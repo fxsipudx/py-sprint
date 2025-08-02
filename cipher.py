b7261c61fc695a1bedbf4897f703a827ff5342e2
Shubham Jena <146214100+fxsipudx@users.noreply.github.com>
2025-08-02 19:49:38 +0200

    made it to a function

--- cipher.py
+++ cipher.py
@@ -1,12 +1,13 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-text = "hello world"
-shift = 3
-answer = ""
-
-for char in text.lower():
-    if char in alphabet:
-        index = alphabet.find(char)
-        new_index = (index + shift) % len(alphabet)
-        answer += alphabet[new_index]
-print(answer)        
+def caesar_cipher(text,shift):
+    answer = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for char in text.lower():
+        if char == " ":
+            answer += " "
+        elif char in alphabet:
+            index = alphabet.find(char)
+            new_index = (index + shift) % len(alphabet)
+            answer += alphabet[new_index]
+    return answer    
+print(caesar_cipher("hello world", 3))        
     
