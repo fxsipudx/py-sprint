bdf2fdf7da1a3233387c8b0a5c34ff94bb08d6ff
Shubham Jena <146214100+fxsipudx@users.noreply.github.com>
2025-08-03 18:27:33 +0200

    Vigenère cipher

--- cipher.py
+++ cipher.py
@@ -1,13 +1,28 @@
-def caesar_cipher(text,shift):
+def caesar_cipher(text,key,direction = 1):
+    key_index = 0
     answer = ""
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     for char in text.lower():
-        if char == " ":
-            answer += " "
+        if not char.isalpha():
+            answer += char
         elif char in alphabet:
+            key_char = key[key_index % len(key)]
+            key_index += 1
+            shift = alphabet.find(key_char)
             index = alphabet.find(char)
-            new_index = (index + shift) % len(alphabet)
+            new_index = (index + direction * shift) % len(alphabet)
             answer += alphabet[new_index]
     return answer    
-print(caesar_cipher("hello world", 3))        
-    
+def encrypt(message,key):
+    return caesar_cipher(message,key)  
+def decrypt(message,key):
+    return caesar_cipher(message,key,-1)
+     
+msg = "Hello, World!"
+key = "abc"
+
+encrypted = encrypt(msg, key)
+print("Encrypted:", encrypted)
+
+decrypted = decrypt(encrypted, key)
+print("Decrypted:", decrypted)
